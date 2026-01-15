e4fc01fea9e56024665049f167b4541b4bf7171d
GabrielPine256 <oxygen.hydrogen565@slmails.com>
2026-01-15 12:35:35 -0800

    Add files via upload

---
+++ compile_linux.sh
@@ -0,0 +1,2 @@
+g++ main.cpp test.hpp curses_board_game/curses_board_game.hpp curses_board_game/board.hpp curses_board_game/hand.hpp curses_board_game/piece.hpp curses_board_game/coord.hpp curses_board_game/dice.hpp curses_board_game/place_among.hpp curses_board_game/to_curses.hpp curses_board_game/args.hpp -o test -lncurses
+
